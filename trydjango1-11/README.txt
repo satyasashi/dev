33268a0e5ee33afe5ac01e42852a7c3642974920
satyasashi <sashirubik@gmail.com>
2018-01-09 02:05:41 +0530

    Modified URL to access DetailView and changed DetailView in views.py

--- trydjango1-11/README.txt
+++ trydjango1-11/README.txt
@@ -1,3 +1,3 @@
-Date: 03-01-2018
-Time: 01:54 AM
-Stopped tryDjango 1.11 video at 01:01 min
+Date: 09-01-2018
+Time: 01:55 AM
+Stopped tryDjango 1.11 video at 02:44:40 min
--- trydjango1-11/src/restaurants/views.py
+++ trydjango1-11/src/restaurants/views.py
@@ -2,7 +2,7 @@
 # This is called - Q Lookups
 from django.db.models import Q
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Importing Class based view
 from django.views import View
@@ -28,6 +28,13 @@ def restaurant_listview(request):
 # Generic - ListView
 class RestaurantListView(ListView):
 
+    # def get_context_data(self, *args, **kwargs):
+    #     print(self.kwargs)
+    #     context = super(RestaurantListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    # This method shows you what Context or data is retreiving and helps you see the 
+    # objects returned. For ListView Default Context will be 'object_list'.
+
     def get_queryset(self):
         slug = self.kwargs.get("slug")
         if slug:
@@ -42,3 +49,23 @@ class RestaurantListView(ListView):
 # Generic - DetailView
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
+
+    # we don't know what Context is coming through by default. So, we mention it.
+    # when you see the results in Terminal, it retreives the {'pk'} passed in URL
+    # and the Object dictionary containing the Data Objects. So, you can use 'object'
+    # to get -- 'name', 'category',...
+    # For DetailView always Default Context will be 'object'
+    def get_context_data(self, *args, **kwargs):
+        print(self.kwargs)
+        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+    # So far we can only call DetailView with Object 'pk' or 'slug'.
+    # If you change <pk> to <some_id> in URLs it shows 'pk' or 'slug' AttributeError.
+    # You can change this to <some_id> which is also Object ID/PK it needs by using
+    # get_object(self, *args, **kwargs). Also Import 'get_object_or_404' shortcut to TryExcept.
+    def get_object(self, *args, **kwargs):
+        rest_id = self.kwargs.get('rest_id') # <whatever_id> you gave in URLs RegexGroup
+        obj = get_object_or_404(RestaurantLocation, pk=rest_id) # OR  id = rest_id
+        return obj
